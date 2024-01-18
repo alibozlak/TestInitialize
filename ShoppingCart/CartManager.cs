0547207e35296fdda01c8add15442c8cba1e5a94
alibozlak <ali-bozlak@outlook.com>
2024-01-18 15:15:56 +0300

    Add project files.

---
+++ ShoppingCart/CartManager.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCart
+{
+    //Gereksinimler : 
+    //1. Sepete ürün eklenebilmelidir.
+    //2. Sepetten ürün çıkarılabilmelidir.
+    //3. Sepet temizlenebilmelidir.
+    public class CartManager
+    {
+        private readonly List<CartItem> _cartItems;
+
+        public CartManager()
+        {
+            _cartItems = new List<CartItem>();
+        }
+
+        public bool Add(CartItem cartItem)
+        {
+            int countOfItems = _cartItems.Count;
+            _cartItems.Add(cartItem);
+            if (_cartItems.Count > countOfItems)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public bool Remove(int productId)
+        {
+            CartItem cartItem = _cartItems.FirstOrDefault(c => c.Product.ProductId == productId);
+            int countOfItems = _cartItems.Count;
+            _cartItems.Remove(cartItem);
+            if (_cartItems.Count < countOfItems)
+                return true;
+            return false;
+        }
+
+        public bool Clear()
+        {
+            _cartItems.Clear();
+            if (_cartItems.Count == 0)
+                return true;
+            return false;
+        }
+
+        public List<CartItem> GetCartItems()
+        {
+            return _cartItems;
+        }
+
+        public int GetCartItemsCount()
+        {
+            return _cartItems.Count;
+        }
+    }
+}
---
+++ ShoppingCartTests/CartManagerTests.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ShoppingCart;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCart.Tests
+{
+    [TestClass()]
+    public class CartManagerTests
+    {
+        private CartManager _cartManager;
+        private CartItem _cartItem;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _cartManager = new CartManager();
+            _cartItem = new CartItem()
+            {
+                Product = new Product()
+                {
+                    ProductId = 1,
+                    ProductName = "Laptop",
+                    UnitPrice = 45_000
+                },
+                Quantity = 8
+            };
+
+            _cartManager.Add(_cartItem);
+        }
+
+        [TestCleanup] 
+        public void TestCleanup()
+        {
+            _cartManager.Clear();
+        }
+
+        [TestMethod()]
+        public void SepeteUrunEklenebilmelidirTest()
+        {
+            //Action 
+            //_cartManager.Add(_cartItem);
+            int countOfCartItems = _cartManager.GetCartItemsCount();
+
+            //Assert 
+            Assert.AreEqual(1, countOfCartItems);
+        }
+
+        [TestMethod()]
+        public void SepettenUrunCikarilabilmelidirTest()
+        {
+            int countOfItemsBeforeRemove = _cartManager.GetCartItemsCount();
+
+            //Action
+            bool isRemoved = _cartManager.Remove(1);
+            int countOfItemsAfterRemove = _cartManager.GetCartItemsCount();
+
+            //Assert
+            Assert.IsTrue(isRemoved);
+            Assert.AreEqual(countOfItemsAfterRemove, countOfItemsBeforeRemove - 1);
+        }
+
+        [TestMethod()]
+        public void SepetTemizlenebilmelidirTest()
+        {
+            //Action 
+            _cartManager.Clear();
+
+            //Assert 
+            Assert.AreEqual(0, _cartManager.GetCartItemsCount());
+        }
+    }
+}
